df7cb16df1d4b6196a07d2dc184dba2fef9c9837
C Brown <chuck@chuckbrown.net>
2014-08-24 12:17:48 -0500

    Submitting Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to cache a potentially
+## computation intensive result so that it can be retrieved
+## when needed instead of having to be computed each time
 
-## Write a short comment describing this function
+## makeCacheMatrix clears an stored value in m, creates a an
+##   object to hold a cached inverted matrix, and defines
+##   functions used by cacheSolve
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() { solve(x) }
+  setmean <- function(mean) { m <<- mean }
+  getmean <- function() { m }
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve accesses the object and returns the mean if it
+##   is cached, or it computes the mean, caches it and returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
